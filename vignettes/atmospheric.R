abf615744ff5cd9de4a57568bfead9eed452ca12
dankelley <kelley.dan@gmail.com>
2024-07-16 07:40:00 -0300

    improve vignette description of plot and problems

--- vignettes/atmospheric.R
+++ vignettes/atmospheric.R
@@ -1,7 +1,7 @@
 ## ----echo = FALSE-------------------------------------------------------------
 knitr::opts_chunk$set(collapse = TRUE, comment = "#>")
 
-## ----echo = TRUE--------------------------------------------------------------
+## ----echo = TRUE, fig.width = 5, fig.height = 5, fig.cap = "Skew-T log-P sounding profile at Shearwater, Nova Scotia, Canada, on July 11, 2024. The wider line represents temperature and the  thinner line represents dew point. The curves are skewed to the right; to read values, trace the 45 degree lines down to the horizontal axis."----
 library(atmosphere)
 file <- system.file("extdata", "Shearwater_2024-07-11.txt.gz", package = "atmosphere")
 sounding <- readAtmosphericSounding(file)
